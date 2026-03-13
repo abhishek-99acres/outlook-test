6f2775f77e82d1c8fa053d0839ef60e459a1f87a
Abhishek Kumar <abhishek.k11@99acres.com>
2026-03-13 12:29:23 +0530

    Command file updated

--- commands.js
+++ commands.js
@@ -255,6 +255,8 @@
 const TRIGGER_RECIPIENT_MAP = {
   "abhishek.a3@99acres.com": "Abhishek Anand",
   "sonia.m@99acres.com": "Sonia M",
+  "coder.abhi02@gmail.com": "Abhishek Kumar",
+  "rkd02122@gmail.com": "Abhishek Kumar",
   "finance@contoso.com": "Finance",
   "legal@contoso.com": "Legal",
   "hr@contoso.com": "HR",
